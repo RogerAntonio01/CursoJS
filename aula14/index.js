0e93fe810742968eca4476bc21bd5120bbba4a4f
Roger Antonio <roger.a.sou@gmail.com>
2024-08-15 11:57:52 -0300

    aula15 JS

--- aula14/index.js
+++ aula14/index.js
@@ -2,7 +2,18 @@ let num1 = 10.45546354635;
 let num2 = 2.5;
 
 // console.log(num1.toString() + num2)  // converte a variavel somente por um momento
+
 // num1 = num1.toString()  // converte a variavel pra sempre
+
 // console.log (typeof num1)  // Tipo da variavel
-//console.log(num1.toFixed(2))  // limita quantas casa decimais terá
-console.log(Number.isInteger())
+
+//console.log(num1.toFixed(2))  // limita quantas casa decimais aparecerá
+
+//console.log(Number.isInteger(num1)) // retorna verdadeiro ou falso se é um numero inteiro
+
+//let temp = null * 'Ola';
+//console.log(Number.isNaN(temp));// retorna verdadeiro se a conta for invalida
+
+num1 = Number(num1.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.54437;
+
+//let num2 = Math.floor(num1); // Arredonda o numero para baixo
+//let num2 = Math.ceil(num1); // Arredonda o numero para cima
+//let num2 = Math.round(num1); // Arredonda para o numero mais proximo
+//console.log(num2);
+
+//console.log(Math.max(1,3,5,7,3,7,9,5,13,45,767,34,656,887,565,39)); // Escreve o maior numero 
+//console.log(Math.min(1,3,5,7,3,7,9,5,13,45,767,34,656,887,565,39)); // Escreve o menor numero
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // gera um numero aleatorio de 0 a 1 mais o 1 nunca é incluido com o round arredondando
+const aleatorio = (Math.random() * (10 - 5) + 5); // gera um numero aleatorio de 0 a 1 mais o 1 nunca é incluido
+console.log(aleatorio)
