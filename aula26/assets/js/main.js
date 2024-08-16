5271203ac609b8bd91d20567f2024a026c93eee4
Roger Antonio <roger.a.sou@gmail.com>
2024-08-16 17:59:14 -0300

    aula26 JS

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,1 +1,17 @@
-alert(1);
+// capturar evento de submit do formulario
+const form = document.querySelector('.formulario');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Evento previnido.');
+    setResultado('Olá mundo')
+});
+
+function setResultado (msg) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = ``;
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado');
+    p.innerHTML = `Qualquer coisa`;
+
+}
