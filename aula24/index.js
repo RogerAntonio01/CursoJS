69a9e20bc1c6f675198fd2f30d3c8945beaf6548
Roger Antonio <roger.a.sou@gmail.com>
2024-08-16 15:17:00 -0300

    aula24 JS

---
+++ aula24/index.js
@@ -0,0 +1,15 @@
+/* 
+Entre 0 - 11 - Bom dia
+entre 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+*/
+
+let hora = 23;
+
+if (hora <= 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
