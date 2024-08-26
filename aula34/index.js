c3f4aa41a77d72de84aa38bb666c1f9e0435893b
Roger Antonio <roger.a.sou@gmail.com>
2024-08-26 14:22:40 -0300

    aula34 JS

---
+++ aula34/index.js
@@ -0,0 +1,17 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i - index
+for (let i = 0; i<=5; i++) {
+    console.log(`Linha ${i}`);
+}
+
+const frutas = ['maçã', 'Pera', 'uva', 'Luiz', 'Otavio', 'João', 'Miranda'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}`, frutas[i]);
+}
