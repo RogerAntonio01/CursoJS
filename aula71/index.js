123dd7449f8fe00b291b8a5621a616d8a51d2e45
Roger Antonio <roger.a.sou@gmail.com>
2024-09-06 09:43:16 -0300

    aula71 JS

---
+++ aula71/index.js
@@ -0,0 +1,20 @@
+// defineProperty -> define uma propriedade por vez
+// defineProperties -> define varias propriedades por vez
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        value: estoque, // valor
+        writable: true, // pode alterar
+        configurable: true // configuravel
+    });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+console.log(Object.keys(p1));
+
+for(let chave in p1) {
+    console.log(chave);
+}
