a318db19faaacf47b06425dd8b99be627b1fdd0e
Roger Antonio <roger.a.sou@gmail.com>
2024-09-17 14:52:39 -0300

    node aula13

---
+++ node/aula10/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+//         Criar   let   atualizar  apagar
+// CRUD -> CREATE, READ, UPDATE,   DELETE
+//         POST    GET   PUT       DELETE
+
+app.get('/', (req, res) => {
+    res.send('<form action= "/" method="POST"> Nome do cliente: <input type="text" name =""><button> Enviar</button></form>'); 
+});
+
+app.use(express.urlencoded({extended: true}));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3001, () => {
+    console.log('Acessar http://localhost:3001');
+    console.log('Servidor executando na porta 3001');
+
+})
